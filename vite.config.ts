448d9e98b8cc0cd22f0d055731bc1b75868992b5
Navineshraj <navineshraj@trudosys.com>
2026-02-27 12:31:00 +0530

    Deploy latest build

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/navi-portfolio/", // 🔥 Required for GitHub Pages
   plugins: [react()],
+  build: {
+    outDir: "docs",   // 🔥 Build output folder
+    emptyOutDir: true // 🔥 Clears old docs before building
+  }
 });
